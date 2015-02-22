fe48a2fdd56c664e1ddeee71674344931774047b
wendy0402 <wendykurniawan92@gmail.com>
2015-02-22 22:59:20 +0700

    create the basic tictactoe rule

--- table.rb
+++ table.rb
@@ -1,23 +1,83 @@
 class Table
+  attr_reader :size, :val_pos
   def initialize(size)
     @size = size < 3 ? 3 : size
 
     @table = ""
-    reset_pos_val
-    @table << create_border << create_content << create_border
+    reset_val_pos
   end
 
   def display
+    render_table
     puts @table
     self
   end
 
   def update_table(opts = {})
-    opts[:location].x
+    loc = location(opts[:coordinate])
+    @val_pos[loc] = opts[:mark]
+    display
+    true
+  end
+
+  def check_grid(coor)
+    coordinate = location(coor)
+
+    if !location_exist?(coordinate)
+      puts "The Grid is not exists, please choose others"
+      return true
+    elsif location_filled?(coordinate)
+      puts "The Grid already filled, please choose others"
+      return true
+    end
+    false
+  end
+
+  def location(opts = {})
+    if opts[:x].to_i > @size || opts[:y].to_i > @size
+      0
+    else
+      row(opts[:x]) + opts[:y].to_i
+    end
+  end
+
+  def location_exist?(loc)
+    @val_pos.has_key?(loc)
+  end
+
+  def total_grids
+    @size ** 2
+  end
+
+  def location_filled?(loc)
+    @val_pos[loc] != " "
+  end
+
+  def coordinate(loc)
+    y = if loc % @size == 0
+      1
+    else
+      loc % @size
+    end
+    { x: loc/@size, y: y }
   end
 
 private
-  def reset_pos_val
+
+  def solution
+    
+  end
+
+  def render_table
+    @table = ""
+    @table << create_border << create_content << create_border
+  end
+
+  def row(x)
+    (x.to_i - 1) * @size
+  end
+
+  def reset_val_pos
     @val_pos ||= {}
     (@size ** 2).times do |n|
       @val_pos[n+1] = " "
@@ -31,6 +91,7 @@ private
   def create_content
     content = ""
     row = 0
+
     @val_pos.each do |key, value|
       if key % @size == 1
         row += 1
@@ -38,6 +99,7 @@ private
       end
       content << "|#{value}"
     end
+
     content << "|\n"
   end
 
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,41 +1,62 @@
 #!/usr/bin/env ruby
-require File.expand_path("../table",__FILE__)
-
+require File.expand_path("../tic_tac_rules", __FILE__)
+require File.expand_path("../table",__FILE__) 
+require File.expand_path("../player",__FILE__)
 class TicTacToe
   def initialize(size = 3)
-    @table = Table.new(size)
+    @table  = Table.new(size)
+    @tic_tac_rule   = TicTacRules.new(size)
   end
 
   def start_game
+    puts "============================="
+    puts "Welcome To Advance TicTacToe"
+    puts "============================="
+    puts "Time to play"
+
+    @player1 = Player.new("X")
+    @player2 = Player.new("O")
+
     @table.display
+    start_turn
+  end
+
+  def stop(mark)
+    puts "============================="
+    puts "CONGRATULATION PLAYER #{mark}"
+    puts "         YOU WON             "
+    puts "============================="
+  end
+
+  def tie
+    puts "============================="
+    puts "     THE RESULT IS TIE       "
+    puts "============================="
+
   end
 
   private
 
   def start_turn
-    turn = 0
-    # loop do
-    #   turn += 1
-    #   player = check_turn(turn)
-    #   location = get_value(turn)
-    #   table.update_table(location: location, sign: player)
-    # end
-  end
+    @table.total_grids.times do |n|
+      player = check_turn(n)
 
-  def get_value(turn)
-    puts "player #{player} turn"
-    puts "location(format: x,y) : "
-    structify_location(gets.chomp.split(','))
+      coordinate = player.move(@table)
+      @table.update_table(coordinate: coordinate, mark: player.mark)
+
+      if @tic_tac_rule.win?(@table, coordinate: coordinate)
+        stop(player.mark)
+        break
+      end
+      if n == @table.total_grids
+        tie
+        break
+      end
+    end
   end
 
   def check_turn(turn)
-    turn.odd? ? "X" : "O"
-  end
-
-  private
-
-  def structify_data(value)
-    Struct.new(x: value[0], y: value[1])
+    turn.odd? ? @player1 : @player2
   end
 end
 
